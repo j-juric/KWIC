eb86806ef9fa663b10886ef62c421a360b92f6a8
j-juric <juric.d.jovan@gmail.com>
2020-01-27 19:15:29 +0100

    Added I/O Media Classes

---
+++ KWIC/InputMedia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWIC
+{
+    class InputMedia
+    {
+        private string inputFile;
+
+        public string InputFile { get => inputFile; set => inputFile = value; }
+
+        public InputMedia() { }
+        public InputMedia(string file) { this.inputFile = file; }
+
+        public string[] GetData()
+        {
+            string text = File.ReadAllText(this.inputFile);
+            string[] lines = text.Split(
+            new[] { "\r\n", "\r", "\n" }
+            , StringSplitOptions.None);            
+            return lines;
+        }       
+    }
+}
---
+++ KWIC/OutputMedia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWIC
+{
+    class OutputMedia
+    {
+        private string outputFile;
+        private string[] data;
+
+        public string OutputFile { get => outputFile; set => outputFile = value; }
+        public string[] Data { get => data; set => data = value; }
+
+        public OutputMedia() { }
+        public OutputMedia(string outputFile, string[] data) { this.outputFile = outputFile; this.data = data; }
+
+        public void OutputData()
+        {
+            File.AppendAllLines("output.txt", this.data);
+        }
+    }
+}
